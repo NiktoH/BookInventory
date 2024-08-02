0aa627dc8e13535c4111aaca68fb7388c7253b27
Салават Садретдинов <sadretdinovs40@gmail.com>
2024-08-02 13:34:09 +0400

    commit1

--- BooksInventory/Main.cpp
+++ BooksInventory/Main.cpp
@@ -9,14 +9,14 @@ private:
 		int* stock;
 		int* wallet;
 public:
-	book() {
+	/*book() {
 		publisher = new char[20];
 		author = new char[20];
 		title = new char[20];
 		price = new float;
 		stock = new int;
 		wallet = new int;
-	}
+	}*/
 	void feeddata();
 	void editdata();
 	void showdata();
@@ -25,21 +25,115 @@ public:
 
 };
 
+int main()
+{
+	book* B[20];
+	int i = 0, t, choice;
+	float w = 0;
+	char authorbuy[20], titlebuy[20];
 
+	while (1) {
+		std::cout << "\n\n\tWELCOME TO LIBRARY!!!";
+		std::cout << "\n\n\tMENU";
+		std::cout << "\n\n1. Entry of new book";
+		std::cout << "\n\n2. Buy book";
+		std::cout << "\n\n3. Search book";
+		std::cout << "\n\n4. Show details of book";
+		std::cout << "\n\n5. EXIT";
+		std::cout << "\n\nEnter ur choice: ";
+		std::cin >> choice;
+
+		switch (choice) {
+		case 1:
+			B[i] = new book;
+			B[i]->feeddata();
+			i++;
+			break;
+		case 2:
+			std::cin.ignore();
+			std::cout << "\nEnter author name: ";
+			std::cin.getline(authorbuy, 20);
+			std::cout << "\nEnter book name: ";
+			std::cin.getline(titlebuy, 20);
+
+			for (t = 0; t < i; t++) {
+				if (B[t]->search(authorbuy, titlebuy)) {
+					B[t]->buybook();
+					break;
+				}
+
+			}
+			if (t == 1)
+				std::cout << "Book is not a stock";
+
+			break;
+		case 3:
+			std::cin.ignore();
+			std::cout << "\nEnter author name: ";
+			std::cin.getline(authorbuy, 20);
+			std::cout << "\nEnter book name: ";
+			std::cin.getline(titlebuy, 20);
+
+			for (t = 0; t < i; t++) {
+				if (B[t]->search(authorbuy, titlebuy)) {
+					B[t]->showdata();
+					break;
+				}
+			}
+			if (t == i)
+				std::cout << "Book has not found";
+
+			break;
+		case 4:
+
+			std::cin.ignore();
+			std::cout << "\nEnter author name: ";
+			std::cin.getline(authorbuy, 20);
+			std::cout << "\nEnter book name: ";
+			std::cin.getline(titlebuy, 20);
+
+			for (t = 0; t < i; t++) {
+				if (B[t]->search(authorbuy, titlebuy)) {
+					B[t]->editdata();
+					break;
+				}
+
+			}
+			if (t == i)
+				std::cout << "Book is not a stock";
+			break;
+
+		case 5:
+			exit(0);
+		default: std::cout << "\nInvalid number of choice!";
+
+		}
+
+	}
+	return 0;
+
+
+}
 
 void book::feeddata() {
 	std::cin.ignore();
 	std::cout << "Enter author of book: ";
+	author = new char[20];
 	std::cin.getline(author, 20);
 	std::cout << "Enter a title: ";
+	title = new char[20];
 	std::cin.getline(title, 20);
 	std::cout << "Enter a publisher: ";
+	publisher = new char[20];
 	std::cin.getline(publisher, 20);
 	std::cout << "Enter price for a book: ";
+	price = new float;
 	std::cin >> *price;
 	std::cout << "Number queue on stock: ";
+	stock = new int;
 	std::cin >> *stock;
 	std::cout << "Enter the amount: ";
+	wallet = new int;
 	std::cin >> *wallet;
 	std::cout << "In ur wallet: " << *wallet;
 }
@@ -96,92 +190,3 @@ void book::buybook() {
   
 }
 
-int main()
-{
-	book* B[20];
-	int i = 0, t, choice;
-	float w = 0;
-	char authorbuy[20], titlebuy[20];
-
-	while (1) {
-	std::cout << "\n\n\tWELCOME TO LIBRARY!!!";
-	std::cout << "\n\n\tMENU";
-	std::cout << "\n\n1. Entry of new book";
-	std::cout << "\n\n2. Buy book";
-	std::cout << "\n\n3. Search book";
-	std::cout << "\n\n4. Show details of book";
-	std::cout << "\n\n5. EXIT";
-	std::cout << "\n\nEnter ur choice: ";
-	std::cin >> choice;
-	
-		switch (choice) {
-		case 1:
-			B[i] = new book;
-			B[i]->feeddata();
-			i++;
-			break;
-		case 2:
-			std::cin.ignore();
-			std::cout << "\nEnter author name: ";
-			std::cin.getline(authorbuy, 20);
-			std::cout << "\nEnter book name: ";
-			std::cin.getline(titlebuy, 20);
-
-			for (t = 0; t < i; t++) {
-				if (B[t]->search(authorbuy, titlebuy)) {
-					B[t]->buybook();
-					break;
-				}
-
-			}
-			if (t == 1) 
-				std::cout << "Book is not a stock";
-			
-			break;
-		case 3:
-			std::cin.ignore();
-			std::cout << "\nEnter author name: ";
-			std::cin.getline(authorbuy, 20);
-			std::cout << "\nEnter book name: ";
-			std::cin.getline(titlebuy, 20);
-
-			for (t = 0; t < i; t++) {
-				if (B[t]->search(authorbuy, titlebuy)) {
-					B[t]->showdata();
-					break;
-				}
-			}
-			if (t == i) 
-				std::cout << "Book has not found";
-			
-			break;
-		case 4:
-
-			std::cin.ignore();
-			std::cout << "\nEnter author name: ";
-			std::cin.getline(authorbuy, 20);
-			std::cout << "\nEnter book name: ";
-			std::cin.getline(titlebuy, 20);
-
-			for (t = 0; t < i; t++) {
-				if (B[t]->search(authorbuy, titlebuy)) {
-					B[t]->editdata();
-					break;
-				}
-
-			}
-			if (t == i) 
-				std::cout << "Book is not a stock";
-				break;
-
-		case 5:
-			exit(0);
-		default: std::cout << "\nInvalid number of choice!";
-			
-		}
-		
-	}
-	return 0;
-
-	
-}
